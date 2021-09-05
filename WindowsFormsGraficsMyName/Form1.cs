a17f7fc341f880ff635bbc7a593bfefc993346f1
Dim Sultanov <dim-sultanov@outlook.com>
2021-09-05 13:06:14 +0500

    Адаптивная надпись

--- WindowsFormsGraficsMyName/Form1.cs
+++ WindowsFormsGraficsMyName/Form1.cs
@@ -12,31 +12,68 @@ namespace WindowsFormsGraficsMyName
 {
     public partial class Form1 : Form
     {
+
+        Bitmap bitmap;
+        Graphics grafics;
+
         public Form1()
         {
             InitializeComponent();
+            
         }
 
-        public void DrawStringFloatFormat(PaintEventArgs e)
+        public void FunctDrawMyName(int width = 0, int hight = 0, string fio="")
         {
+            /*
+             * point = 50;
+             * width = 500;
+             * height = 120;
+             * 
+             * pointW = 96;
+             * pointH = 76;
+             * 
+             */
 
-            // Create string to draw.
-            String drawString = "Sample Text";
+            this.BackColor = Color.White;
+            float font;
+            float fontW;
+            float fontH;
 
-            // Create font and brush.
-            Font drawFont = new Font("Arial", 16);
-            SolidBrush drawBrush = new SolidBrush(Color.Black);
+            if (width < 0 || hight < 0)
+                return;
 
-            // Create point for upper-left corner of drawing.
-            float x = 150.0F;
-            float y = 50.0F;
+            int lenWord = fio.Length;
 
-            // Set format of string.
-            StringFormat drawFormat = new StringFormat();
-            drawFormat.FormatFlags = StringFormatFlags.DirectionVertical;
+            fontW = width / 10;
+            fontH = hight / 3;
 
-            // Draw string to screen.
-            e.Graphics.DrawString(drawString, drawFont, drawBrush, x, y, drawFormat);
+            if (fontW > fontH)
+                font = fontH;
+            else
+                font = fontW;
+
+            Font fnt = new System.Drawing.Font("Arial", font);
+            Brush br = new SolidBrush(Color.Red);
+            Point pt = new Point(0, 0);
+
+            grafics = this.CreateGraphics();
+            grafics.Clear(Color.White);
+            grafics.DrawString(fio, fnt, br, pt);
+
+            SizeF stringSize = new SizeF();
+            stringSize = grafics.MeasureString(fio, fnt);
+
+            int t = (int)(Math.Floor(this.ClientSize.Height / 2.0) - (fnt.Height/2));
+            int top = t < 0 || t + fnt.Height >= this.ClientSize.Height ? 0 : t;
+            int l = (int)Math.Ceiling((this.Width - (int)stringSize.Width) / 2.0);
+            int left = l < 0 || l + (int)stringSize.Width >= this.ClientSize.Width ? 0 : l;
+
+            pt = new Point(left, top);
+            grafics.Clear(Color.White);
+            grafics.DrawString(fio, fnt, br, pt);
+
+            //MessageBox.Show($"{ this.ClientSize.Height } {t + fnt.Height}");
+
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -44,19 +81,45 @@ namespace WindowsFormsGraficsMyName
 
         }
 
-        struct MyName
-        {
-            string FIO;
-        }
-
         private void Form1_Paint(object sender, PaintEventArgs e)
         {
-            Font fnt = new System.Drawing.Font("Arial", (float)50);
-            Brush br = new SolidBrush(Color.Red);
-            Point pt = new Point(100, 100);
-            Graphics g = e.Graphics;
-            g.DrawString("Текст", fnt, br, pt);
-            FontFamily[] a = FontFamily.Families;
+            
+            FunctDrawMyName(this.Width, this.Height, "Султанов Дим");
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void Form1_ResizeBegin(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_ResizeEnd(object sender, EventArgs e)
+        {
+            FunctDrawMyName(this.Width, this.Height, "Султанов Дим");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Shown(object sender, EventArgs e)
+        {
+            
+        }
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+
+        }
+
+        private void toolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+
         }
     }
 }
